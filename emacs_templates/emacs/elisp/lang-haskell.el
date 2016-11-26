6b20b423c60ab88407a7fa5c6ea37ee171f4a41b
Chakravarthy Raghunandan <rnraghunandan@gmail.com>
2016-11-26 23:29:29 +0530

    modify lang-haskell.el to use intero instead of relying on company-ghci

--- emacs_templates/emacs/elisp/lang-haskell.el
+++ emacs_templates/emacs/elisp/lang-haskell.el
@@ -1,22 +1,24 @@
-(use-package flycheck-haskell
-  :config
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  (add-hook 'flycheck-mode-hook 'flycheck-haskell-configure))
-
-(use-package company-ghci
-  :config
-  (push 'company-ghci company-backends))
-
+;; haskell-mode configuration
+;; https://github.com/haskell/haskell-mode
 (use-package haskell-mode
+  :ensure t	     
+  ;; haskell-mode swaps `C-m' and `C-j' behavior. Revert it back
+  :bind (:map haskell-mode-map
+              ("C-m" . newline)
+              ("C-j" . electric-newline-and-maybe-indent))
   :config
-  (add-hook 'haskell-mode-hook 'haskell-doc-mode)
-  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
+  (defun my-haskell-mode-hook ()
+    "Hook for `haskell-mode'."
+    (set (make-local-variable 'company-backends)
+         '((company-intero company-files))))
+  (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
   (add-hook 'haskell-mode-hook 'company-mode)
-  (add-hook 'haskell-mode-hook (lambda ()
-				 (set (make-local-variable 'company-backends)
-				      (append '((company-capf company-dabbrev-code))
-					      company-backends)))))
+  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+
+  ;; intero-mode for a complete IDE solution to haskell
+  ;; commercialhaskell.github.io/intero
+  (use-package intero
+    :ensure t	       
+    :config (add-hook 'haskell-mode-hook 'intero-mode)))
 
 (provide 'lang-haskell)
