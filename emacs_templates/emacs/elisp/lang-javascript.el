844708c3e0ce61aa9c86dbef57ca27d6e18edfd5
Chakravarthy Raghunandan <rnraghunandan@gmail.com>
2016-11-27 18:01:05 +0530

    Add tern and js2-refactor
    Refactored code
    Added keybindings for `js2-refactor`
    Added configuration for `tern-mode` which provides company-completion for JS

--- emacs_templates/emacs/elisp/lang-javascript.el
+++ emacs_templates/emacs/elisp/lang-javascript.el
@@ -1,4 +1,10 @@
+;; js2-mode
+;; https://github.com/mooz/js2-mode
 (use-package js2-mode
+  :ensure t
+  :mode
+  ("\\.js$" . js2-mode)
+  ("\\.json$" . js2-jsx-mode)           
   :config
   (custom-set-variables '(js2-strict-inconsistent-return-warning nil))
   (custom-set-variables '(js2-strict-missing-semi-warning nil))
@@ -6,13 +12,36 @@
   (setq js-indent-level 2)
   (setq js2-indent-level 2)
   (setq js2-basic-offset 2)
-  (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.js$" . js2-jsx-mode)))
 
-(use-package js2-refactor)
+  ;; tern :- IDE like features for javascript and completion
+  ;; http://ternjs.net/doc/manual.html#emacs
+  (use-package tern
+    :config
+    (defun my-js-mode-hook ()
+      "Hook for `js-mode'."
+      (set (make-local-variable 'company-backends)
+           '((company-tern company-files))))
+    (add-hook 'js2-mode-hook 'my-js-mode-hook)
+    (add-hook 'js2-mode-hook 'company-mode))
 
-(use-package js-comint
-  :config
-  (setq inferior-js-program-command "node"))
+  (add-hook 'js2-mode-hook 'tern-mode)
+
+  ;; company backend for tern
+  ;; http://ternjs.net/doc/manual.html#emacs
+  (use-package company-tern)
+
+  ;; Run a JavaScript interpreter in an inferior process window
+  ;; https://github.com/redguardtoo/js-comint
+  (use-package js-comint
+    :ensure t             
+    :config
+    (setq inferior-js-program-command "node"))
+
+  ;; js2-refactor :- refactoring options for emacs
+  ;; https://github.com/magnars/js2-refactor.el
+  (use-package js2-refactor :defer t
+    :diminish js2-refactor
+    :bind ("C-c j r" . js2r-add-keybindings-with-prefix)))
+
 
 (provide 'lang-javascript)
