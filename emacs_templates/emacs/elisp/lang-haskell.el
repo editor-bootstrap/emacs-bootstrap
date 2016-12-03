16f9a5acf5d51e539b47eda2a5b5bafa12584468
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-03 11:38:03 +0530

    format haskell, rust buffers on save

--- emacs_templates/emacs/elisp/lang-haskell.el
+++ emacs_templates/emacs/elisp/lang-haskell.el
@@ -1,7 +1,6 @@
 ;; haskell-mode configuration
 ;; https://github.com/haskell/haskell-mode
 (use-package haskell-mode
-  :ensure t	     
   ;; haskell-mode swaps `C-m' and `C-j' behavior. Revert it back
   :bind (:map haskell-mode-map
               ("C-m" . newline)
@@ -18,7 +17,15 @@
   ;; intero-mode for a complete IDE solution to haskell
   ;; commercialhaskell.github.io/intero
   (use-package intero
-    :ensure t	       
-    :config (add-hook 'haskell-mode-hook 'intero-mode)))
+    :config (add-hook 'haskell-mode-hook 'intero-mode))
+
+  ;; hindent - format haskell code automatically
+  ;; https://github.com/chrisdone/hindent
+  (use-package hindent
+    :diminish hindent-mode
+    :config
+    (add-hook 'haskell-mode-hook #'hindent-mode)
+    ;; reformat the buffer using hindent on save
+    (setq hindent-reformat-buffer-on-save t)))
 
 (provide 'lang-haskell)
